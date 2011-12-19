9d3fef48c3a0927745eea383b1e70b2ea9bd8f46
Kirill Silin <kyrylosilin@gmail.com>
2011-12-19 05:30:15 +0200

    Fix automatic requiring of sources

--- lib/patience.rb
+++ lib/patience.rb
@@ -1,5 +1,7 @@
 require 'ray'
 require 'forwardable'
 
-Dir['patience/scenes/*.rb'].each { |scene| require_relative scene }
-Dir['patience/*.rb'].each { |file| require_relative file }
+Dir.chdir(File.dirname(__FILE__)) do
+  Dir['patience/scenes/*.rb'].each { |scene| require_relative scene }
+  Dir['patience/*.rb'].each { |file| require_relative file }
+end
