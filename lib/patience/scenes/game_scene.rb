e51321c328012575fd57e6042b7b0cfaef12d0aa
Kirill Silin <kyrylosilin@gmail.com>
2011-12-26 07:44:14 +0200

    Whoops, adjust nesting

--- lib/patience/scenes/game_scene.rb
+++ lib/patience/scenes/game_scene.rb
@@ -1,40 +1,40 @@
 module Patience
   class GameScene < Ray::Scene
 
-  def setup
-    @background_color = Ray::Color.new(31, 95, 25)
-    @deck = Deck.new
-    a = 0
-    @deck.cards.each do |card|
-      card.sprite.pos += [a, 0]
-      a += 20
-    end
-  end
-
-  def register
-    add_hook :quit, method(:exit!)
-    add_hook :key_press, key(:q), method(:exit!)
-
-    always do
-      on :mouse_press do
-        @clicked_card = @deck.cards.find { |card| card.received_click?(mouse_pos) }
-        @clicked_card.pick_up(mouse_pos) if @clicked_card
+    def setup
+      @background_color = Ray::Color.new(31, 95, 25)
+      @deck = Deck.new
+      a = 0
+      @deck.cards.each do |card|
+        card.sprite.pos += [a, 0]
+        a += 10
       end
+    end
 
-      on :mouse_release do
-        @clicked_card = nil
+    def register
+      add_hook :quit, method(:exit!)
+      add_hook :key_press, key(:q), method(:exit!)
+
+      always do
+        on :mouse_press do
+          @clicked_card = @deck.cards.find { |card| card.received_click?(mouse_pos) }
+          @clicked_card.pick_up(mouse_pos) if @clicked_card
+        end
+
+        on :mouse_release do
+          @clicked_card = nil
+        end
+
+        @clicked_card.drag(mouse_pos) if @clicked_card
       end
-
-      @clicked_card.drag(mouse_pos) if @clicked_card
     end
-  end
 
-  def render(win)
-    win.clear(@background_color)
-    @deck.cards.each do |card|
-      win.draw(card.sprite)
+    def render(win)
+      win.clear(@background_color)
+      @deck.cards.each do |card|
+        win.draw(card.sprite)
+      end
     end
-  end
 
-end
+  end
 end
