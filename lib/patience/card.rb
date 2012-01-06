fca9162e3c48baf1ace324bf06c3f8cb00feaf70
Kirill Silin <kyrylosilin@gmail.com>
2012-01-06 19:26:39 +0200

    Add new methods, which operate card's face and back and add some comments on methods

--- lib/patience/card.rb
+++ lib/patience/card.rb
@@ -17,20 +17,56 @@ module Patience
       @rank   = rank
       @suit   = suit
       @sprite = Ray::Sprite.new path_of('patience/sprites/card_deck.png')
+      # A sheet with 13 columns and 5 rows. First
+      # row and column corresponds to the card back.
       @sprite.sheet_size = [13, 5]
       @sprite.sheet_pos = [rank, suit]
     end
 
+    # Print human readable rank and suit of
+    # the card. For example: "ace of spades".
     def to_s
       "#{RANKS[@rank]} of #{SUITS[@suit-1]}"
     end
 
+    # Check whether card is "red" (being
+    # in suit of either hearts or diamonds).
     def red?
       suit == 1 or suit == 2
     end
 
+    # Check whether card is "black" (being
+    # in suit of either spades or clubs).
+    # The opposite of #red? method.
     def black?
       !self.red?
+    end
+
+    # Turn a card to its face.
+    def face_up
+      sprite.sheet_pos = [@rank, @suit]
+    end
+
+    # Check if a card is turned to its face.
+    # The opposite of #face_down? method.
+    def faced_up?
+      !self.faced_down?
+    end
+
+    # Turn a card to its back.
+    def face_down
+      sprite.sheet_pos = [0, 0]
+    end
+
+    # Check if a card is turned to its back.
+    def faced_down?
+      sprite.sheet_pos == [0, 0]
+    end
+
+    # Either turn a card to its face if it's faced
+    # down or turn it to its back if it's faced up.
+    def flip
+      (face_up if faced_down?) or (face_down if faced_up?)
     end
 
   end
