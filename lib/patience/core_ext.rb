0bb8b849329da01054569332e7ab669dfcbd0df6
Kirill Silin <kyrylosilin@gmail.com>
2012-01-14 18:57:49 +0200

    Monkey patch String with constantize from ActiveSupport

--- lib/patience/core_ext.rb
+++ lib/patience/core_ext.rb
@@ -10,6 +10,26 @@ class String
     end
 
   end
+
+  rake_extension("constantize") do
+
+    # Constantize tries to find a declared constant with the name specified
+    # in the string. It raises a NameError when the name is not in CamelCase
+    # or is not initialized.
+    # Example:
+    #   "Module".constantize #=> Module
+    #   "Class".constantize  #=> Class
+    #   "yadda".constantize  #=> NameError: yadda is not a valid constant name!
+    #
+    def constantize
+      unless /\A(?:::)?([A-Z]\w*(?:::[A-Z]\w*)*)\z/ =~ self
+        raise NameError, "#{self.inspect} is not a valid constant name!"
+      end
+
+      Object.module_eval("::#{$1}", __FILE__, __LINE__)
+    end
+
+  end
 end
 
 class Class
