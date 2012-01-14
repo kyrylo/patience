7c1e4d54a5f52b4faf654836f85e6c00feccad62
Kirill Silin <kyrylosilin@gmail.com>
2012-01-14 14:46:49 +0200

    From now, classes can show their descendant

--- lib/patience/core_ext.rb
+++ lib/patience/core_ext.rb
@@ -1,7 +1,6 @@
 require 'rake'
 
 class String
-
   rake_extension("demodulize") do
 
     # Removes the module part from the
@@ -11,5 +10,20 @@ class String
     end
 
   end
+end
 
+class Class
+  rake_extension("descendants") do
+
+    # Returns the array of all descendants of a class. Although there is
+    # faster way to do this, I want to stick with this solutions, since
+    # it's much simpler to understand and performance isn't crucial for now.
+    #
+    # The example of faster implementation lives in:
+    # rails/activesupport/lib/active_support/descendants_tracker.rb
+    def descendants
+      ObjectSpace.each_object(::Class).select {|klass| klass < self }
+    end
+
+  end
 end
