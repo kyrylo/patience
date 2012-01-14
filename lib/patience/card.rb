c2710ec00865c6ca91c4cb31110db6c2babbea2a
Kirill Silin <kyrylosilin@gmail.com>
2012-01-14 15:49:46 +0200

    Rewrite suits and ranks, add some comments on the stuff

--- lib/patience/card.rb
+++ lib/patience/card.rb
@@ -1,58 +1,138 @@
 module Patience
   ###
-  # Patience::Card is a card creator class. Cards have ranks and suits. Every
-  # card has its own sprite. Sprite is a one big image, that contains all cards.
-  # A sprite for a card is chosen by shift on the sprite. Shift is defined by
-  # integer values on X and Y axis repsectively.
+  # Patience::Card is a card creator class. Cards have ranks and suits.
+  # Every card has its own sprite. Sprite is a one big image, that contains
+  # all cards. A sprite for a card is chosen by shift on the sprite. Shift
+  # is defined by integer values on X and Y axis repsectively.
   class Card
     ###
-    # Suit supplies only one method: #red?. The idea is that every actual suit
-    # has its own class, which is inherited from Suit. Each suit class defines
-    # its own #black? method dynamically.
+    # Rank class provides underlying methods for every rank.
+    class Rank
+      ##
+      # This lambda defines new class, designed to be a child of Rank.
+      create_rank_class = lambda {
+        Class.new(Rank) do
+          def initialize(num)
+            @num = num
+          end
+
+          # Returns integer representation of a rank. Basically, it's
+          # the position of a card in the ascending row of card ranks.
+          # Example:
+          #   card = Card.new(13, 3) #=> Ace of Spades
+          #   card.rank.to_i #=> 13
+          #
+          def to_i
+            num
+          end
+
+          # Returns string representation of a rank. It asks class to
+          # give its full name, exscinding everything but its actual name.
+          # Example:
+          #   card = Card.new(13, 3) #=> Ace of Spades
+          #   card.rank.to_s #=> "Ace"
+          #
+          def to_s
+            "#{self.class.name.demodulize}"
+          end
+        end
+      }
+
+      # Dynamically create rank classes.
+      %w[Two Three Four Five Six Seven
+         Eight Nine Ten Jack Queen King Ace].each do |class_name|
+        Rank.const_set(class_name, create_rank_class.call)
+      end
+    end
+
+    ###
+    # Suit supplies only one method: #red?. The idea is that every
+    # actual suit has its own class, which is inherited from Suit.
+    # Each suit class defines its own #black? method dynamically.
     class Suit
+      ##
+      # This lambda defines new class, designed to be a child
+      # of Suit and supplied with #black? method, contents of
+      # which depends on the given argument "true_or_false".
+      create_suit_class = lambda { |true_or_false|
+        Class.new(Suit) do
+
+          def initialize(num)
+            @num = num
+          end
+
+          # Returns boolean value. For red suits the value
+          # is "false". For black suits the value is "true".
+          # Example:
+          #   card = Card.new(13, 3) #=> Ace of Spades
+          #   card.suit.black? #=> true
+          #
+          define_method :black? do
+            true_or_false
+          end
+
+          # Returns integer representation of a suit.
+          # Example:
+          #   card = Card.new(13, 3) #=> Ace of Spades
+          #   card.suit.to_i #=> 3
+          #
+          def to_i
+            num
+          end
+
+          # Returns plural string representation of a suit. It asks class to
+          # give its full name, exscinding everything but its actual name.
+          # Example:
+          #   card = Card.new(13, 3) #=> Ace of Spades
+          #   card.suit.to_s #=> "Spades"
+          #
+          def to_s
+            "#{self.class.name.demodulize}s"
+          end
+
+        end
+      }
+
+      # Create classes for every suit, giving the answer on the question about
+      # their blackness. If the answer is negative, obviously the suit is red.
+      Heart   = create_suit_class.call(false)
+      Diamond = create_suit_class.call(false)
+      Spade   = create_suit_class.call(true)
+      Club    = create_suit_class.call(true)
+
       # Checks whether card is "red" (being not black).
       def red?; not black?; end
     end
-
-    # This lambda defines new class, designed to be a child
-    # of Suit and supplied with #black? method, contents of
-    # which depends on the given argument "true_or_false".
-    create_suit_class = lambda { |true_or_false|
-      Class.new(Suit) do
-        # Returns boolean value. For red suits the value
-        # is "false". For black suits the value is "true".
-        define_method :black? do
-          true_or_false
-        end
-
-        # Returns plural string representation of the suit.
-        def to_s
-          "#{self.class.name.demodulize}s"
-        end
-      end
-    }
-
-    # Create classes for every suit.
-    Heart   = create_suit_class.call(false)
-    Diamond = create_suit_class.call(false)
-    Spade   = create_suit_class.call(true)
-    Club    = create_suit_class.call(true)
 
     extend Forwardable
     def_delegators :@sprite, :pos, :x, :y
 
     attr_reader :rank, :suit, :sprite
 
-    SUITS = [:hearts, :diamonds, :spades, :clubs]
-    RANKS = [2, 3, 4, 5 ,6 ,7 ,8 ,9, 10, :jack, :queen, :king, :ace]
-
     def initialize(rank, suit)
-      @rank   = rank
+      # Supply every suit and rank with its numerical alias.
+      @rank = case rank
+              when 1  then Rank::Two.new    1
+              when 2  then Rank::Three.new  2
+              when 3  then Rank::Four.new   3
+              when 4  then Rank::Five.new   4
+              when 5  then Rank::Six.new    5
+              when 6  then Rank::Seven.new  6
+              when 7  then Rank::Eight.new  7
+              when 8  then Rank::Nine.new   8
+              when 9  then Rank::Ten.new    9
+              when 10 then Rank::Jack.new  10
+              when 11 then Rank::Queen.new 11
+              when 12 then Rank::King.new  12
+              when 13 then Rank::Ace.new   13
+              else raise DefunctRank, "Nonexistent rank: #{rank}"
+              end
       @suit = case suit
-              when 1 then Heart.new
-              when 2 then Diamond.new
-              when 3 then Spade.new
-              when 4 then Club.new
+              when 1 then Suit::Heart.new   1
+              when 2 then Suit::Diamond.new 2
+              when 3 then Suit::Spade.new   3
+              when 4 then Suit::Club.new    4
+              else raise DefunctSuit, "Nonexistnet suit: #{suit}"
               end
       @sprite = Ray::Sprite.new path_of('patience/sprites/card_deck.png')
       # A sheet with 13 columns and 5 rows. First
@@ -61,10 +141,13 @@ module Patience
       @sprite.sheet_pos = [rank, suit]
     end
 
-    # Prints human readable rank and suit of
-    # the card. For example: "ace of spades".
+    # Prints human readable rank and suit of the card.
+    # Example:
+    #   ace = Card.new(13, 3)
+    #   ace.to_s #=> "Ace of Spades"
+    #
     def to_s
-      "#{RANKS[@rank]} of #{suit}"
+      "#{rank} of #{suit}"
     end
 
     # Turns a card to its face.
@@ -94,5 +177,7 @@ module Patience
       (face_up if face_down?) or (face_down if face_up?)
     end
 
+    class DefunctRank < StandardError; end
+    class DefunctSuit < StandardError; end
   end
 end
