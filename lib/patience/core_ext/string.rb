01d1490371be4688f5ac24c2e8dde6d8454aa698
Kirill Silin <kyrylosilin@gmail.com>
2012-01-14 23:32:02 +0200

    Split up code from to core_ext.rb to separate files

--- lib/patience/core_ext/string.rb
+++ lib/patience/core_ext/string.rb
@@ -31,19 +31,3 @@ class String
 
   end
 end
-
-class Class
-  rake_extension("descendants") do
-
-    # Returns the array of all descendants of a class. Although there is
-    # faster way to do this, I want to stick with this solutions, since
-    # it's much simpler to understand and performance isn't crucial for now.
-    #
-    # The example of faster implementation lives in:
-    # rails/activesupport/lib/active_support/descendants_tracker.rb
-    def descendants
-      ObjectSpace.each_object(::Class).select {|klass| klass < self }
-    end
-
-  end
-end
