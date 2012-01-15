a8c08568185c299741ebe840a407618f95e101af
Kirill Silin <kyrylosilin@gmail.com>
2012-01-15 16:19:18 +0200

    Fix up tests, again

--- test/patience/test_deck.rb
+++ test/patience/test_deck.rb
@@ -6,11 +6,13 @@ module Patience
     def setup
       @deck = Deck.new
       @cards = @deck.cards.map(&:to_s)
-      @all_suits = ['hearts', 'diamonds', 'spades', 'clubs']
+      @ranks = %w[Two Three Four Five Six Seven Eight Nine Ten]
+      @suits = %w[Hearts Diamonds Spades Clubs]
     end
 
     def assert_deck_includes_all_suits_of(rank)
-      @all_suits.each { |suit| assert_includes @cards, "#{rank} of #{suit}" }
+      rank = @ranks[rank-2] unless rank.instance_of? String
+      @suits.each { |suit| assert_includes @cards, "#{rank} of #{suit}" }
     end
 
     def test_deck_can_be_created
@@ -21,63 +23,33 @@ module Patience
       assert_equal 52, @deck.size
     end
 
-    def test_initial_deck_has_deuces_of_each_suit
-      assert_deck_includes_all_suits_of 2
-    end
-
-    def test_initial_deck_has_triples_of_each_suit
-      assert_deck_includes_all_suits_of 3
-    end
-
-    def test_initial_deck_has_fours_of_each_suit
-      assert_deck_includes_all_suits_of 4
-    end
-
-    def test_initial_deck_has_fives_of_each_suit
-      assert_deck_includes_all_suits_of 5
-    end
-
-    def test_initial_deck_has_sixes_of_each_suit
-      assert_deck_includes_all_suits_of 6
-    end
-
-    def test_initial_deck_has_sevens_of_each_suit
-      assert_deck_includes_all_suits_of 7
-    end
-
-    def test_initial_deck_has_eights_of_each_suit
-      assert_deck_includes_all_suits_of 8
-    end
-
-    def test_initial_deck_has_nines_of_each_suit
-      assert_deck_includes_all_suits_of 9
-    end
-
-    def test_initial_deck_has_tens_of_each_suit
-      assert_deck_includes_all_suits_of 10
+    (2..10).each do |n|
+      define_method "test_initial_deck_has_#{n}_of_each_suit" do
+        assert_deck_includes_all_suits_of n
+      end
     end
 
     def test_initial_deck_has_jacks_of_each_suit
-      assert_deck_includes_all_suits_of 'jack'
+      assert_deck_includes_all_suits_of 'Jack'
     end
 
     def test_initial_deck_has_queens_of_each_suit
-      assert_deck_includes_all_suits_of 'queen'
+      assert_deck_includes_all_suits_of 'Queen'
     end
 
     def test_initial_deck_has_kings_of_each_suit
-      assert_deck_includes_all_suits_of 'king'
+      assert_deck_includes_all_suits_of 'King'
     end
 
     def test_initial_deck_has_aces_of_each_suit
-      assert_deck_includes_all_suits_of 'ace'
+      assert_deck_includes_all_suits_of 'Ace'
     end
 
     def test_cards_of_a_deck_can_be_accessed_via_its_position_in_the_deck
-      assert_equal 'ace of spades',  @cards[50]
-      assert_equal '2 of clubs',     @cards[3]
-      assert_equal '10 of diamonds', @cards[33]
-      assert_equal 'jack of hearts', @cards[36]
+      assert_equal 'Ace of Spades',   @cards[50].to_s
+      assert_equal 'Two of Clubs',    @cards[3].to_s
+      assert_equal 'Ten of Diamonds', @cards[33].to_s
+      assert_equal 'Jack of Hearts',  @cards[36].to_s
     end
 
   end
