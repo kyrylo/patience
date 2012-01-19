114c3134bfcf61d7339f24eb3930dd0753176d8d
Kirill Silin <kyrylosilin@gmail.com>
2012-01-20 00:58:28 +0200

    Add Area class

---
+++ lib/patience/area.rb
@@ -0,0 +1,43 @@
+module Patience
+  ###
+  # Patience::Area provides area objects, that consist of piles. The goal of
+  # this class is to assemble piles into a logical bundle with some common
+  # methods, so they could be controlled via only one interface.
+  class Area
+    attr_reader :piles
+
+    def initialize(cards=[], piles_num=1)
+      @piles = []
+      @cards = Pile.new(cards)
+      piles_num.times { @piles << Pile.new }
+    end
+
+    # Returns the array of position of the area. Such
+    # is considered as the position of the first pile.
+    def pos
+      piles.first.pos
+    end
+
+    # Sets the position of every pile in the area.
+    def pos=(pos)
+      piles.each { |pile| pile.pos = *pos }
+    end
+
+    # Draws area in the window.
+    def draw_on(win)
+      piles.each { |pile| pile.draw_on(win) }
+    end
+
+    # Finds all cards in every pile and returns array of these cards.
+    def cards
+      cards = @piles.inject([]) { |cards, pile| cards << pile.cards  }
+      cards.flatten
+    end
+
+    # Returns the card, which has been clicked (if there's been any).
+    def card_at(mouse_pos)
+      cards.reverse.find { |card| card.sprite.to_rect.contain?(mouse_pos) }
+    end
+
+  end
+end
