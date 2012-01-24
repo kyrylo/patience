a9d3410a40d2c13a039b5bf6c4ea4d7dbbce3f44
Kirill Silin <kyrylosilin@gmail.com>
2012-01-24 14:06:57 +0200

    Load event handlers

--- lib/patience.rb
+++ lib/patience.rb
@@ -6,7 +6,8 @@ def path_of(resource)
 end
 
 Dir.chdir(File.dirname(__FILE__)) do
-  Dir['patience/scenes/*.rb'].each { |scene| require_relative scene }
+  Dir['patience/event_handlers/*.rb'].each { |eh| require_relative eh }
   Dir['patience/core_ext/*.rb'].each { |ext| require_relative ext }
+  Dir['patience/scenes/*.rb'].each { |scene| require_relative scene }
   Dir['patience/*.rb'].each { |file| require_relative file }
 end
