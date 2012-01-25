9054ca42f59f7e4f172a2477c8c3b145be60cd64
Kirill Silin <kyrylosilin@gmail.com>
2012-01-26 00:25:52 +0200

    Cursor class get lessen and add #active?

--- lib/patience/cursor.rb
+++ lib/patience/cursor.rb
@@ -3,45 +3,17 @@ module Patience
   # Patience::Cursor handles operations with mouse. The whole point of this is
   # to provide dragging of cards.
   class Cursor
-    attr_accessor :mouse_pos
-    attr_reader :obj
+    attr_accessor :mouse_pos, :click
 
-    # Sets obj to given object.
-    def click=(obj)
-      @obj = obj
-    end
-
-    # Returns true if there is any object. Otherwise returns false.
-    def clickable?
-      obj.not.nil?
-    end
-
-    # Changes position of the obj regarding mouse position.
-    def drag
-      obj.sprite.pos = mouse_pos + @offset
-    end
-
-    # Returns the offset: the value of the
-    # difference of obj's position and current mouse position.
-    def pick_up
-      @offset = obj.sprite.pos - mouse_pos
-    end
-
-    # Sets obj to nil. That means, that you would be
-    # able to use none of the instance methods of Cursor.
+    # Sets click to the state, when nothing was clicked.
     def drop
-      @obj = nil
+      @click = nil
     end
 
-    # Returns true if the obj responds to sprite. Otherwise returns false.
-    def drawable?
-      obj.respond_to?(:sprite)
-    end
-
-    # Just a syntactic sugar for more readability. Returns the result of the
-    # pickable? metod. If you can draw it, then you also can pick it, don't you?
-    def pickable?
-      drawable?
+    # Returns true, if cursor clicked anything
+    # but non-game objects. Otherwise returns false.
+    def active?
+      click and click.not.nothing?
     end
 
   end
