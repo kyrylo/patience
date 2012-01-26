f0a9ddaf604ecb9ebe6bd27942e8a9abdac6751b
Kirill Silin <kyrylosilin@gmail.com>
2012-01-26 14:06:18 +0200

    Refactor Click handlers

--- lib/patience/event_handlers/click/information_processor.rb
+++ lib/patience/event_handlers/click/information_processor.rb
@@ -35,16 +35,42 @@ module Patience
           info.to_h[:area].instance_of? Stock
         end
 
+        def stock_scenario
+          Proc.new do
+            empty_stock = 'patience/sprites/empty_stock.png'
+            if pile.size == 1
+              pile.background = Ray::Sprite.new path_of(empty_stock)
+            end
+            card.face_up
+            @areas[:waste].piles.first << exempt(card)
+          end
+        end
+
         def waste?
           info.to_h[:area].instance_of? Waste
+        end
+
+        def waste_scenario
+          Proc.new do
+          end
         end
 
         def tableau?
           info.to_h[:area].instance_of? Tableau
         end
 
+        def tableau_scenario
+          Proc.new do
+          end
+        end
+
         def foundation?
           info.to_h[:area].instance_of? Foundation
+        end
+
+        def foundation_scenario
+          Proc.new do
+          end
         end
 
       end
