918a8ab7ffa05c233edf11ee080f87af7d112665
Kirill Silin <kyrylosilin@gmail.com>
2012-01-26 19:04:07 +0200

    Remove unnecessary method

--- lib/patience/area.rb
+++ lib/patience/area.rb
@@ -12,12 +12,6 @@ module Patience
       piles_num.times { @piles << Pile.new }
     end
 
-    # Returns the array of position of the area. Such
-    # is considered as the position of the first pile.
-    def pos
-      piles.first.pos
-    end
-
     # Sets the position of every pile in the area.
     def pos=(pos)
       piles.each { |pile| pile.pos = *pos }
@@ -32,11 +26,6 @@ module Patience
     def cards
       cards = @piles.inject([]) { |cards, pile| cards << pile.cards  }
       cards.flatten
-    end
-
-    # Returns the card, which has been clicked (if there's been any).
-    def card_at(mouse_pos)
-      cards.find { |card| card.hit?(mouse_pos) }
     end
 
   end
