4de99274b6c61ba9e462f432ce1be3c356cc9014
Kirill Silin <kyrylosilin@gmail.com>
2012-01-31 01:59:06 +0200

    Rewrite tests for Cursor

--- test/patience/test_cursor.rb
+++ test/patience/test_cursor.rb
@@ -4,84 +4,87 @@ module Patience
   class TestCursor < MiniTest::Unit::TestCase
 
     def setup
-      @cursor   = Cursor.new
-      @object   = Card.new(13, 3)
+      @cursor = Cursor.new
+      @card = Card.new(13, 3)
+      areas = { :area => Area.new }
+      areas[:area].piles.first << @card
+      offset = Ray::Vector2.new(20, 20)
       mouse_pos = Ray::Vector2.new
-      @cursor.click = @object
       @cursor.mouse_pos = mouse_pos
+      @cursor.click = EventHandler::Click.new(mouse_pos, areas)
+      @cursor.drag = EventHandler::Drag.new(@card, offset)
     end
 
     def test_cursor_can_be_created
       assert Cursor.new
     end
 
-    def test_cursor_responds_to_methods
-      [:mouse_pos, :mouse_pos=, :obj, :click=,
-      :clickable?, :drag, :pick_up, :drop, :drawable?,
-      :pickable?].each { |method| assert_respond_to @cursor, method }
+    def test_cursor_responds_to_instance_methods
+      methods = [:mouse_pos, :mouse_pos=, :click, :click=, :drag,
+                 :drag=, :drop, :clicked_something?, :still_on_something?,
+                 :movable?, :carrying_card?, :drawable?]
+      methods.each { |method| assert_respond_to @cursor, method }
     end
 
-    def test_click_on_the_object_works
-      assert_equal "Ace of Spades", @cursor.obj.to_s
-    end
-
-    def test_cursor_can_check_whether_the_object_is_clickable
-      cursor = @cursor.dup
-      assert cursor.clickable?
-      cursor = Cursor.new
-      refute cursor.clickable?
-    end
-
-    def test_cursor_can_drag_objects
-      cursor = @cursor.dup
-      cursor.pick_up
-      assert_equal Ray::Vector2.new(0, 0), cursor.obj.pos
-
-      cursor.mouse_pos += [20, 20]
-      cursor.drag
-      assert_equal Ray::Vector2.new(20, 20), cursor.obj.pos
-
-      cursor.mouse_pos += [1000, 1]
-      cursor.drag
-      assert_equal Ray::Vector2.new(1020, 21), cursor.obj.pos
-
-      cursor.mouse_pos -= [0, 800]
-      cursor.drag
-      assert_equal Ray::Vector2.new(1020, -779), cursor.obj.pos
-    end
-
-    def test_cursor_can_pick_up_objects
-      cursor = @cursor.dup
-      assert_equal Ray::Vector2.new(0, 0), cursor.pick_up
-      cursor.mouse_pos += [20, 20]
-      assert_equal Ray::Vector2.new(-20, -20), cursor.pick_up
-      cursor.mouse_pos += [100, 100]
-      assert_equal Ray::Vector2.new(-120, -120), cursor.pick_up
-      cursor.mouse_pos -= [300, 300]
-      assert_equal Ray::Vector2.new(180, 180), cursor.pick_up
+    def test_cursor_responds_to_delegated_methods
+      delegated_methods = [:card, :pile, :offset, :draggable?]
+      delegated_methods.each { |method| assert_respond_to @cursor, method }
     end
 
     def test_cursor_can_drop_objects
       cursor = @cursor.dup
-      assert cursor.obj
+      assert cursor.click
+      assert cursor.drag
       cursor.drop
-      refute cursor.obj
+      refute cursor.click
+      refute cursor.drag
     end
 
-    def test_cursor_can_check_whether_clicked_object_can_be_drawn
+    def test_cursor_can_check_whether_it_clicked_something
       cursor = @cursor.dup
-      assert cursor.drawable?
+      assert cursor.clicked_something?
       cursor.drop
-      cursor.click = Array.new
-      refute cursor.drawable?
+      refute cursor.clicked_something?
     end
 
-    def test_cursor_can_check_whether_clicked_card_can_be_picked
+    def test_cursor_can_check_whether_it_still_hovers_clicked_object
       cursor = @cursor.dup
-      assert cursor.pickable?
+      assert cursor.still_on_something?
+
+      areas = { :area => Area.new }
+      areas[:area].piles.first << @card
+      areas
+      @cursor.click = EventHandler::Click.new(cursor.mouse_pos, areas)
+      assert cursor.still_on_something?
+
+      cursor.mouse_pos = Ray::Vector2.new(1000, 1000)
+      refute cursor.still_on_something?
+
+      cursor.card.sprite.pos = cursor.mouse_pos
+      assert cursor.still_on_something?
+
+    end
+
+    def test_cursor_can_check_if_the_object_is_movable
+      cursor = @cursor.dup
+      assert cursor.movable?
+      cursor.card.face_down
+      refute cursor.movable?
       cursor.drop
-      cursor.click = Array.new
-      refute cursor.pickable?
+      cursor.movable?
+    end
+
+    def test_cursor_can_check_whether_it_is_carrying_a_card
+      cursor = @cursor.dup
+      assert cursor.carrying_card?
+
+      cursor.drop
+      refute cursor.carrying_card?
+
+      areas = { :area => Area.new }
+      areas[:area].piles.first << @card
+      @cursor.click = EventHandler::Click.new(cursor.mouse_pos, areas)
+      refute cursor.carrying_card?
     end
 
   end
