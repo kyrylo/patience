17a39518baf23a55def55a11203bc7a6571b692d
Kirill Silin <kyrylosilin@gmail.com>
2012-01-31 02:03:18 +0200

    Amend tests for Version

--- test/patience/test_version.rb
+++ test/patience/test_version.rb
@@ -1,34 +1,10 @@
 require_relative 'helper'
 
 module Patience
-  class TestVersion < MiniTest::Unit::TestCase
+  class TestVERSION < MiniTest::Unit::TestCase
 
-    def test_version_has_major_number
-      assert Version::MAJOR
-    end
-
-    def test_major_number_is_fixnum
-      assert_instance_of Fixnum, Version::MAJOR
-    end
-
-    def test_version_has_minor_number
-      assert Version::MINOR
-    end
-
-    def test_minor_number_is_fixnum
-      assert_instance_of Fixnum, Version::MINOR
-    end
-
-    def test_version_has_tiny_number
-      assert Version::TINY
-    end
-
-    def test_tiny_number_is_fixnum
-      assert_instance_of Fixnum, Version::TINY
-    end
-
-    def test_version_numbers_can_be_concatenated_together
-      assert_instance_of String, Version::STRING
+    def test_version_is_correct
+      assert_equal "0.0.0", Patience::VERSION, "Patience version isn't correct"
     end
 
   end
