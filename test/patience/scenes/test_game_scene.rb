b048d1ebdba554cdcb0391c50b6eda54e0bf280c
Kirill Silin <kyrylosilin@gmail.com>
2012-02-15 15:08:00 +0200

    Remove clutter

--- test/patience/scenes/test_game_scene.rb
+++ test/patience/scenes/test_game_scene.rb
@@ -3,32 +3,11 @@ require_relative '../helper'
 module Patience
   class TestGameScene < MiniTest::Unit::TestCase
 
-    def test_game_scene_has_background_color
-      skip "Try to write me, in the first place!"
-    end
-
-    def test_game_scene_holds_deck
-      skip "Try to write me, in the first place!"
-    end
-
-    def test_game_scene_can_be_exited_via_hooks
-      skip "Try to write me, in the first place!"
-    end
-
-    def test_game_scene_can_register_mouse_presses
-      skip "Try to write me, in the first place!"
-    end
-
-    def test_game_scene_can_register_mouse_releases
-      skip "Try to write me, in the first place!"
-    end
-
-    def test_game_scene_can_draw_background
-      skip "Try to write me, in the first place!"
-    end
-
-    def test_game_scene_can_draw_cards
-      skip "Try to write me, in the first place!"
+    def setup
+      @game = Patience::Game.new
+      @scene = @game.registered_scene(:game_scene)
+      @scene.register
+      @scene.setup
     end
 
   end
