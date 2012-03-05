10abcf599a172a5ede7e18b4244f7d378f191629
Kirill Silin <kyrylosilin@gmail.com>
2012-03-05 23:03:09 +0200

    Tidy up comments and rename #select_in to #detect_in

--- lib/patience/processable.rb
+++ lib/patience/processable.rb
@@ -1,27 +1,25 @@
 module Patience
-  ###
-  # Processable module.
   module Processable
     attr_reader :area, :pile, :card
 
-    # Finds the area or pile, or card (depends on the option)
-    # in the areas. If option is wrong, raises ArgumentError.
-    def select_in(areas, option, &blk)
+    # Finds area or pile, or card (depends on the option) in
+    # the areas. If the option is wrong, raises ArgumentError.
+    def detect_in(areas, option, &block)
       case option
-        when :area then find_area_in(areas, &blk)
-        when :pile then find_pile_in(areas, &blk)
-        when :card then find_card_in(areas, &blk)
+        when :area then find_area_in(areas, &block)
+        when :pile then find_pile_in(areas, &block)
+        when :card then find_card_in(areas, &block)
       else
         raise ArgumentError, "Unknown option: #{option}"
       end
     end
 
-    # Returns the area, which is being clicked.
+    # Returns area, which is being clicked.
     def find_area_in(areas)
       areas.values.find { |area| yield(area) }
     end
 
-    # Returns the pile, which is being clicked.
+    # Returns pile, which is being clicked.
     def find_pile_in(areas)
       find_area_in(areas) do |area|
         pile = area.piles.find { |pile| yield(pile) }
@@ -30,7 +28,7 @@ module Patience
       nil
     end
 
-    # Returns the card, which is being clicked.
+    # Returns card, which is being clicked.
     def find_card_in(areas)
       find_pile_in(areas) do |pile|
         card = pile.cards.reverse.find { |card| yield(card) }
@@ -39,57 +37,50 @@ module Patience
       nil
     end
 
-    # Returns the array, containing gathered hit elements.
+    # Returns array, containing gathered hit elements.
     def to_a
       [area, pile, card]
     end
 
-    # Returns the hash, containing gathered hit elements.
+    # Returns hash, containing gathered hit elements.
     def to_h
       { :area => area, :pile => pile, :card => card }
     end
 
-    # Returns true, if there hasn't been found
-    # something. Otherwise, returns false.
+    # Returns true, if there hasn't been found something.
     def nothing?
       to_a.compact.size.zero?
     end
 
-    # Returns true, if there's been found something.
-    # Otherwise, returns false. Opposite of #nothing?.
+    # Returns true, if there's been found something. Opposite of #nothing?.
     def something?
       not nothing?
     end
 
-    # Returns subtraction between card and mouse position at the moment
-    # of click, only if cursor clicked the card. Otherwise, returns nil.
+    # Returns subtraction between card and mouse position at
+    # the moment of click, only if cursor clicked the card.
     def pick_up(card, mouse_pos)
-      card.pos - mouse_pos if card
+      card.pos - mouse_pos
     end
 
-    # Returns true, if the clicked area is Stock. Otherwise, returns false.
+    # Returns true, if the clicked area is Stock.
     def stock?
       area.instance_of? Stock
     end
 
-    # Returns true, if the clicked area is Waste. Otherwise, returns false.
+    # Returns true, if the clicked area is Waste.
     def waste?
       area.instance_of? Waste
     end
 
-    # Returns true, if the clicked area is Tableau. Otherwise, returns false.
+    # Returns true, if the clicked area is Tableau.
     def tableau?
       area.instance_of? Tableau
     end
 
-    # Returns true, if the clicked area is Foundation. Otherwise, returns false.
+    # Returns true, if the clicked area is Foundation.
     def foundation?
       area.instance_of? Foundation
-    end
-
-    # Deletes card from the pile and returns it. It is used in scenarios.
-    def exempt(card)
-      pile.cards.delete(card)
     end
 
   end
